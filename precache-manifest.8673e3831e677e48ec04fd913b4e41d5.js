dca90d51eaf2d974ca435bd23bdac9d7c5fee464
Elber <elberrd@gmail.com>
2020-07-03 00:31:52 -0400

    Updates

---
+++ precache-manifest.8673e3831e677e48ec04fd913b4e41d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e62000c50a8a96cf6c4211acb3864c8e",
+    "url": "/first_react_project/index.html"
+  },
+  {
+    "revision": "466c5047eff63deb6f3b",
+    "url": "/first_react_project/static/css/main.86c048a5.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/first_react_project/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first_react_project/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "466c5047eff63deb6f3b",
+    "url": "/first_react_project/static/js/main.cbf0d9e6.chunk.js"
+  },
+  {
+    "revision": "32b4828aca1b1483c446",
+    "url": "/first_react_project/static/js/runtime-main.1d6d8591.js"
+  }
+]);
